474761807a3de34c05abc55b8b13d6fe8e362f94
CodeWorld3 <lthompson1206@gmail.com>
2021-12-22 22:03:56 -0500

    bare bones and props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2021, 2, 28);
+    
+   
+
+    return (
+        <div className="expense-item">
+            <div>{props.date}</div>
+{/* <div>{props.date}</div> */}
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItem;
